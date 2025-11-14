8863ddd69b1cc735d9b58468799abe2f89ef64ef
jesspav <202656197+jesspav@users.noreply.github.com>
2025-11-13 17:14:13 -0800

    precommit

--- rust/sedona-raster-functions/src/register.rs
+++ rust/sedona-raster-functions/src/register.rs
@@ -37,9 +37,10 @@ pub fn default_function_set() -> FunctionSet {
     }
 
     register_scalar_udfs!(
-        function_set, 
+        function_set,
         crate::rs_size::rs_width_udf,
-    crate::rs_size::rs_height_udf,);
+        crate::rs_size::rs_height_udf,
+    );
 
     register_aggregate_udfs!(function_set,);
 
