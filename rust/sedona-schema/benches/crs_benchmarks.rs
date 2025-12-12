ac56e00ca1d7995f730c148083d78c1ee290c814
jesspav <202656197+jesspav@users.noreply.github.com>
2025-12-12 08:46:15 -0800

    add bench

---
+++ rust/sedona-schema/benches/crs_benchmarks.rs
@@ -0,0 +1,59 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
+use sedona_schema::crs::deserialize_crs;
+
+fn bench_crs_equality(c: &mut Criterion) {
+    c.bench_function("equality_lnglat_crs", |b| {
+        b.iter(|| {
+            for _ in 0..100000 {
+                let crs_json1: serde_json::Value = serde_json::from_str("\"EPSG:4326\"").unwrap();
+                let crs_json2: serde_json::Value = serde_json::from_str("\"OGC:CRS84\"").unwrap();
+                let crs1 = deserialize_crs(&crs_json1)
+                    .unwrap()
+                    .unwrap();
+                let crs3 = deserialize_crs(&crs_json2)
+                    .unwrap()
+                    .unwrap();
+                let eq = crs1.crs_equals(crs3.as_ref());
+                black_box(eq);
+            }
+        })
+    });
+
+    c.bench_function("equality_different_crs", |b| {
+        b.iter(|| {
+                for _ in 0..100000 {
+                let crs_json1: serde_json::Value = serde_json::from_str("\"EPSG:3827\"").unwrap();
+                let crs_json2: serde_json::Value = serde_json::from_str("\"EPSG:3857\"").unwrap();
+                let crs1 = deserialize_crs(&crs_json1)
+                    .unwrap()
+                    .unwrap();
+                let crs2 = deserialize_crs(&crs_json2)
+                    .unwrap()
+                    .unwrap();
+                let eq = crs1.crs_equals(crs2.as_ref());
+                black_box(eq);
+            }
+        })
+    });
+}
+
+criterion_group!(benches, bench_crs_equality);
+criterion_main!(benches);
